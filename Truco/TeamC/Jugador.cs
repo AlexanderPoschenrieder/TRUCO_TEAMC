3bdbb89543b7cad5220f332ee112c73bcdd960d8
Adriano Pacini <apacini@souhtplans.com.ar>
2016-07-22 11:49:16 -0300

    Contestar y cantar envido

--- Truco/TeamC/Jugador.cs
+++ Truco/TeamC/Jugador.cs
@@ -27,6 +27,40 @@ namespace Truco
             return Accion.truco;
         }
 
+        public override Accion ContestarEnvido(Param param, int tantos)
+        {
+            Accion accion = Accion.noquiero_tanto;
+            Accion cantorival = ObtenerUltimoCantoRival(param);
 
+            if (param.juego.quienEsMano == Quienesmano.Rival)
+            {
+                if (cantorival == Accion.envido && tantos <= 25) accion = Accion.noquiero_tanto;
+                if (cantorival == Accion.envido && tantos >= 26 && tantos <= 28) accion = Accion.quiero_tanto;
+                if (cantorival == Accion.envido && (tantos == 29 || tantos == 30)) accion = Accion.envidoenvido;
+                if (cantorival == Accion.envido && tantos > 30) accion = Accion.realenvido;
+
+                if (cantorival == Accion.realenvido && tantos >= 25 && tantos >= 28) accion = Accion.quiero_tanto;
+                if (cantorival == Accion.realenvido && tantos >= 19) accion = Accion.quiero_tanto;
+
+                //if (cantorival == Accion.faltaenvido && tantos > 30) accion = Accion.quiero_tanto;
+
+                //if (cantorival == Accion.envidoenvido && tantos > 28) accion = Accion.quiero_tanto;
+                //if (cantorival == Accion.envidorealenvido && tantos > 29) accion = Accion.quiero_tanto;
+                //if (cantorival == Accion.envidofaltaenvido && tantos > 30) accion = Accion.quiero_tanto;
+                //if (cantorival == Accion.realenvidofaltaenvido && tantos > 30) accion = Accion.quiero_tanto;
+            }
+
+            return accion;
+        }
+
+        public override Accion CantarEnvido(Param param, int tantos)
+        {
+            Accion accion = Accion.nulo;
+            if (param.AccionesDisponibles.Contains(Accion.envido) && tantos >= 25 && tantos <= 29) accion = Accion.envido;
+            if (param.AccionesDisponibles.Contains(Accion.realenvido) && tantos >= 29) accion = Accion.realenvido;
+            //if (param.AccionesDisponibles.Contains(Accion.faltaenvido) && tantos > 30) accion = Accion.faltaenvido;
+
+            return accion;
+        }
     }
 }
